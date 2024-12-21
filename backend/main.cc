7420061475e728c2f39ca96a5d4b890e73e092eb
tanglong3bf <tanglong3bf@163.com>
2024-12-21 23:48:19 +0800

    添加了两个依赖：DrogonJwtUtil、Buldrokkas_tee，并设置了 jwt 的密钥

--- backend/main.cc
+++ backend/main.cc
@@ -1,7 +1,20 @@
 #include <drogon/drogon.h>
+#include <JwtUtil.h>
+#include <Buldrokkas_tee.h>
+
+using namespace drogon;
+using namespace tl::jwt;
 
 int main()
 {
+    app().registerBeginningAdvice([]() {
+        auto jwtUtil = app().getPlugin<JwtUtil>();
+        if (!jwtUtil)
+        {
+            LOG_ERROR << "JwtUtil 插件获取失败，请检查配置文件";
+        }
+        jwtUtil->setSecret("FrostNova");
+    });
     // Load config file
     drogon::app().loadConfigFile("../config.yaml");
     // Run HTTP framework,the method will block in the internal event loop
